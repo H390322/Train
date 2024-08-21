74abec74630b1ab9ac58b6b77977d0ec96f61af9
H390322 <sattawat.kongmee@michelin.com>
2024-08-21 16:03:36 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import seaborn as sns
+from sklearn.neighbors import KNeighborsRegressor
+
+x = 4*np.random.rand(100)
+y = np.sin(2*x + 1) + 0.1* np.random.randn(100)
+
+knn = KNeighborsRegressor(n_neighbors=1)
+knn.fit(x.reshape(-1, 1), y)
 
