bcab5ef2b304e02cbe6fe0a5900d8cbd2839cf66
H390322 <sattawat.kongmee@michelin.com>
2024-08-21 14:49:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
 df = sns.load_dataset('iris')
 df
@@ -9,21 +13,17 @@ y = df['species']
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(x_train, y_train)
-y_pred = knn.predict(x_test)
-
-st.sidebar.title('Classification')
-classification = st.sidebar.selectbox('Select classification', ['KNN', 'SVM'])
+st.sidebar.title('Classifiers')
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
 if classifier == 'KNN':
   knn = KNeighborsClassifier(n_neighbors=3)
   knn.fit(x_train, y_train)
   y_pred = knn.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
   st.write(acc)
 if classifier == 'SVM':
   svm = SVC()
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
-  accuracy_score(y_test, y_pred)
+  acc = accuracy_score(y_test, y_pred)
   st.write(acc)
